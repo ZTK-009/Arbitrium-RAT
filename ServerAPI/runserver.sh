bc16700ba9c7273969841db7a8e3519d7d5510e7
Ayoub <ayoub1benchaliah@gmail.com>
2021-01-18 00:22:17 +0100

    force usage of python2.7

--- ServerAPI/runserver.sh
+++ ServerAPI/runserver.sh
@@ -1,5 +1,5 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
-screen -S main -d -m bash -c "python main.py"
-screen -S initproxy -d -m bash -c "python initProxy.py"
+screen -S main -d -m bash -c "python2.7 main.py"
+screen -S initproxy -d -m bash -c "python2.7 initProxy.py"
 echo "[+] the Arbitrium-Server is running ..."
